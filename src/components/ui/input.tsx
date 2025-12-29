49d66db5a25704bbf9349cdd942d2bc333c4eff2
Charlie Del Grande <charliedelgrande@gmail.com>
2025-12-28 19:43:16 -0500

    fixed zooming

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-11 w-full rounded-lg border border-border bg-transparent px-3 py-2 text-sm outline-none ring-offset-background placeholder:text-muted-foreground focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          // Use >=16px font-size to prevent iOS Safari zoom-on-focus.
+          "flex h-11 w-full rounded-lg border border-border bg-transparent px-3 py-2 text-[16px] outline-none ring-offset-background placeholder:text-muted-foreground focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
